39ce4e9b870ff42c6741cf1cf5ddc815063b414d
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-10-29 15:31:58 +0100

    [command_base] Added a feedback publisher

--- ros/src/ropod_experiment_executor/commands/command_base.py
+++ ros/src/ropod_experiment_executor/commands/command_base.py
@@ -1,11 +1,16 @@
 from __future__ import print_function
 from abc import abstractmethod
+import rospy
+
+from ropod_ros_msgs.msg import CommandFeedback
 
 class CommandBase(object):
     def __init__(self, name):
         self.name = name
         self._executing = False
         self._preempted = False
+        self.cmd_feedback_topic = rospy.get_param('/cmd_feedback_topic', '/ropod/cmd_feedback')
+        self.feedback_pub = rospy.Publisher(self.cmd_feedback_topic, CommandFeedback, queue_size=10)
 
     @abstractmethod
     def execute(self, params=None):
@@ -23,3 +28,6 @@ class CommandBase(object):
         print('[{0}] Cancelling command'.format(self.name))
         self._preempted = True
         self._executing = False
+
+    def send_feedback(self, feedback_msg):
+        self.feedback_pub.publish(feedback_msg)
