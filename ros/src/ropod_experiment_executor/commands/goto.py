bcbed140664f69bcb0daf8a6a51e86155f5255ed
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-03-27 19:07:22 +0100

    [goto] Changed the stopping condition based on the status feedback

--- ros/src/ropod_experiment_executor/commands/goto.py
+++ ros/src/ropod_experiment_executor/commands/goto.py
@@ -31,6 +31,9 @@ class GoTo(CommandBase):
         self.area_list = []
         self.current_area_idx = 0
 
+        self.waypoint_counter = 0
+        self.number_of_waypoints = 0
+
         # wait for a while to give the action publisher time to initialise
         rospy.sleep(0.1)
 
@@ -67,6 +70,8 @@ class GoTo(CommandBase):
             area_msg.sub_areas.append(subarea_msg)
             action_msg.areas.append(area_msg)
 
+        self.number_of_waypoints = len(self.areas)
+
         print('[{0}] Going to {1}'.format(self.name, self.area_list[self.current_area_idx]))
         self.go_to_action_pub.publish(action_msg)
         self.action_completed = False
@@ -88,8 +93,10 @@ class GoTo(CommandBase):
         '''Processes a navigation action progress message and modifies the value of
         self.action_completed depending on the message status code.
         '''
-        if progress_msg.status.status_code == Status.COMPLETED:
-            self.action_completed = True
+        if progress_msg.status.status_code == Status.REACHED and progress_msg.sequenceNumber > 0:
+            self.waypoint_counter += 1
+            if self.waypoint_counter == self.number_of_waypoints:
+                self.action_completed = True
         elif progress_msg.status.status_code == Status.ONGOING:
             self.current_area_idx += 1
             print('[{0}] Going to {1}'.format(self.name, self.area_list[self.current_area_idx]))
